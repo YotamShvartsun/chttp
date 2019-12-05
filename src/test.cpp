71deaee994a931695d98d9eaf8ab732266865e12
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2019-12-05 10:19:20 +0200

    tested HTTPresponse + built MultipartField

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,10 @@
-#include "Tests/GetRequestTest.h"
+#include <chttp/util/MultipartField.h>
 
 int main(int argc, char **argv) {
-    TestGet();
+    std::string ds = "---a\r\nContent-Disposition: form-data; name=\"file1\"; filename=\"a.txt\"\r\nContent-Type: text/plain\r\n\r\naaa\r\n-----a";
+    std::vector<char> d(ds.begin(), ds.end());
+    std::string ct = "multipart/form-data; boundary=---a";
+    std::vector<char> c(ct.begin(), ct.end());
+    MultipartField f(d, ct);
     return 0;
 }
