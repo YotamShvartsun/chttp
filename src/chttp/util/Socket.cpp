84c51c45f681efff3b455d7b7e1caaed32190232
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2019-12-16 13:11:10 +0200

    Added the GetData method7

--- src/chttp/util/Socket.cpp
+++ src/chttp/util/Socket.cpp
@@ -6,7 +6,7 @@ Socket::Socket() {
 #endif
     this->sockfd = socket(AF_INET, SOCK_STREAM, 0);
 #ifdef __linux__
-    if(this->sockfd < 0)
+    if (this->sockfd < 0)
         throw std::runtime_error("Unable to create socket!");
 #else
     if(this->sockfd == INVALID_SOCKET)
@@ -24,32 +24,56 @@ Socket::Socket(SOCKET s) {
 
 void Socket::Bind(int port) {
     struct sockaddr_in serv_addr{};
-    bzero((char*) &serv_addr, sizeof(serv_addr));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = port;
-    if(bind(this->sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
+    if (bind(this->sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         throw std::runtime_error("Unable to bind");
     this->isBinded = true;
 }
 
 void Socket::Listen() {
-    if(!this->isBinded)
+    if (!this->isBinded)
         throw std::logic_error("Can't listen without bind!");
     /*
      * 128 is the default number in /proc/sys/net/core/somaxconn,
      * and according to `listen` man, a bigger number will change to the number in this file
      */
-    if(listen(this->sockfd, 128) < 0)
+    if (listen(this->sockfd, 128) < 0)
         throw std::runtime_error("Unable to listen");
 }
 
 Socket Socket::Accept() {
-    return Socket();
+    SOCKET sock = accept(this->sockfd, NULL, NULL);
+    return Socket(sock);
 }
 
 std::vector<char> Socket::GetData(uint size) {
-    return std::vector<char>();
+    std::vector<char> res;
+    char tmp;
+    int recvCode;
+    if (this->sockfd < 0) {
+#ifdef DEBUG
+        std::cerr << "Trying to read from closed socket!" << std::endl;
+#endif
+        return res;
+    }
+    for (int i = 0; i < size; i++) {
+#ifdef  __linux__
+        if ((recvCode = recv(this->sockfd, &tmp, 1, 0)) < 0) {
+#ifdef DEBUG
+            std::cerr << "Error on GetData. Sockfd:" << this->sockfd << " Socket object in " << std::hex << this;
+            std::cerr << strerror(errno) << std::endl;
+#endif
+        } else if (recvCode == 0) {
+            this->sockfd = -1;
+            return res; // socket closed!
+        } else {
+            res.push_back(tmp);
+        }
+#endif
+    }
 }
 
 void Socket::SendData(std::vector<char> data) {
