f9fbfbb2c321f1c2b089afc3ea35dc4dbb99992a
Yotam Shvartsun <yotam.shvartsun@gmail.com>
2020-05-04 16:15:57 +0200

    Working on the example site backend

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,6 +3,7 @@
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <json.hpp>
 
+#define DB_FILE "exampleBlogDB.db"
 
 struct blogPostJSON {
     int id{};
@@ -10,11 +11,13 @@ struct blogPostJSON {
     std::string body;
 };
 
-void to_json(nlohmann::json& j, const blogPostJSON& p) {
-    j = nlohmann::json{{"title", p.title}, {"body", p.body}, {"id", p.id}};
+void to_json(nlohmann::json &j, const blogPostJSON &p) {
+    j = nlohmann::json{{"title", p.title},
+                       {"body",  p.body},
+                       {"id",    p.id}};
 }
 
-void from_json(const nlohmann::json& j, blogPostJSON& p) {
+void from_json(const nlohmann::json &j, blogPostJSON &p) {
     j.at("id").get_to(p.id);
     j.at("title").get_to(p.title);
     j.at("body").get_to(p.body);
@@ -32,13 +35,12 @@ void Signup(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp
     std::string password = body["password"].get<std::string>();
     nlohmann::json responseBody;
     try {
-        SQLite::Database db("exampleBlogDB.db");
+        SQLite::Database db(DB_FILE, SQLite::OPEN_READWRITE);
         SQLite::Statement insertNewUser(db, "insert into users(username, password) values (?,?)");
         insertNewUser.bind(1, username);
         insertNewUser.bind(2, password);
-        while (insertNewUser.executeStep()) {
-            responseBody["id"] = db.getLastInsertRowid();
-        }
+        insertNewUser.exec();
+        responseBody["id"] = db.getLastInsertRowid();
         resp->AddCookie("Auth", responseBody.dump());
     } catch (std::exception &e) {
         responseBody["error"] = "Unable to create a new user!";
@@ -68,7 +70,7 @@ void Login(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp)
     std::string password = body["password"].get<std::string>();
 
     try {
-        SQLite::Database db("exampleBlogDB.db");
+        SQLite::Database db(DB_FILE);
         SQLite::Statement stmt(db, "select id from users where username = ? and password = ?");
         stmt.bind(1, username);
         stmt.bind(2, password);
@@ -96,7 +98,7 @@ void CreatePost(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> 
     if (cookies.IsInJar("Auth")) {
         try {
             nlohmann::json cookieBody = nlohmann::json::parse(cookies.GetCookieValue("Auth"));
-            SQLite::Database db("exampleBlogDB.db", SQLite::OPEN_READWRITE);
+            SQLite::Database db(DB_FILE, SQLite::OPEN_READWRITE);
             SQLite::Statement isUser(db, "select count(*) as count from users where id=?");
             bool isLoggedIn = false;
             int tmp = cookieBody["UID"].get<int>();
@@ -136,16 +138,17 @@ void CreatePost(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> 
     resp->Raw(respBody.dump());
 }
 
-void AllPosts(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp){
+void AllPosts(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp) {
     nlohmann::json responseBody;
-    try{
-        SQLite::Database db("exampleBlogDB.db");
-        SQLite::Statement allPosts(db, "select posts.post_content, posts.post_title, posts.id, users.username from posts join users on publisher_id=users.id");
+    try {
+        SQLite::Database db(DB_FILE);
+        SQLite::Statement allPosts(db,
+                                   "select posts.post_content, posts.post_title, posts.id, users.username from posts join users on publisher_id=users.id");
         nlohmann::json postsJSON = nlohmann::json::array();
         blogPostJSON postTmp;
         std::string titleTmp;
         std::vector<blogPostJSON> posts;
-        while(allPosts.executeStep()){
+        while (allPosts.executeStep()) {
             postTmp.body = allPosts.getColumn(0).getString();
             titleTmp = allPosts.getColumn(1).getString();
             titleTmp += " - " + allPosts.getColumn(3).getString();
@@ -155,7 +158,7 @@ void AllPosts(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> re
         }
         postsJSON = nlohmann::json(posts);
         responseBody["posts"] = postsJSON;
-    }catch(std::exception &e){
+    } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
         resp->SetStatus(Not_Found);
         responseBody["error"] = "Unable to fetch posts";
@@ -164,6 +167,128 @@ void AllPosts(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> re
     resp->Raw(responseBody.dump());
 }
 
+nlohmann::json FetchPostsByUID(long UID) {
+    nlohmann::json result;
+    try {
+        SQLite::Database db(DB_FILE);
+        SQLite::Statement postsByUID(db, "select post_content, post_title, id from posts where publisher_id=?");
+        postsByUID.bind(1, UID);
+        nlohmann::json postsJSON = nlohmann::json::array();
+        blogPostJSON postTmp;
+        std::vector<blogPostJSON> posts;
+        while (postsByUID.executeStep()) {
+            postTmp.body = postsByUID.getColumn(0).getString();
+            postTmp.title = postsByUID.getColumn(1).getString();
+            postTmp.id = postsByUID.getColumn(2).getInt();
+            posts.push_back(postTmp);
+        }
+        result["posts"] = posts;
+    } catch (...) {
+        result.clear();
+        result["error"] = "Something went wrong";
+    }
+    return result;
+}
+
+void GetPostsByUID(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp) {
+    int UID = std::stoi(req->GetUrlParam("UID"));
+    nlohmann::json respBody = FetchPostsByUID(UID);
+    if (respBody.contains("error")) {
+        resp->SetStatus(Internal_Server_Error);
+    } else if (respBody["posts"].empty()) {
+        try{
+            SQLite::Database db(DB_FILE);
+            SQLite::Statement isUser(db, "select count(*) as count from users where id=?");
+            isUser.bind(1, UID);
+            int tmp = 0;
+            bool userFound = false;
+            while(isUser.executeStep()) {
+                tmp = isUser.getColumn("count");
+                if (tmp != 0) {
+                    userFound = true;
+                }
+            }
+            if(!userFound){
+                respBody.clear();
+                respBody["error"] = "User not found";
+                resp->SetStatus(Not_Found);
+            }
+        } catch (...) {
+            resp->SetStatus(Internal_Server_Error);
+            respBody.clear();
+            respBody["error"] = "Something went wrong";
+        }
+    }
+    resp->Header("Content-Type", "application/json");
+    resp->Raw(respBody.dump());
+}
+
+void DeletePostByID(std::shared_ptr<HttpRequest> req, std::shared_ptr<HttpResponse> resp) {
+    int postID = std::stoi(req->GetUrlParam("postID"));
+    CookieJar cookies(req->GetHeaders()["cookie"]);
+    nlohmann::json responseBody;
+    if (cookies.IsInJar("Auth")) {
+        try {
+            nlohmann::json cookieBody = nlohmann::json::parse(cookies.GetCookieValue("Auth"));
+            int UID = cookieBody["UID"].get<int>();
+            SQLite::Database db(DB_FILE, SQLite::OPEN_READWRITE);
+            SQLite::Statement isUser(db, "select count(*) as count from users where id=?");
+            bool isLoggedIn = false;
+            int tmp = 0;
+            isUser.bind(1, UID);
+            while (isUser.executeStep()) {
+                tmp = isUser.getColumn("count");
+                if (tmp != 0) {
+                    isLoggedIn = true;
+                }
+            }
+            if (isLoggedIn) {
+                SQLite::Statement postExists(db, "select count(*) as count from posts where id=?");
+                bool isPost = false;
+                postExists.bind(1, postID);
+                while (postExists.executeStep()) {
+                    tmp = postExists.getColumn("count");
+                    if (tmp != 0) {
+                        isPost = true;
+                    }
+                }
+                if (isPost) {
+                    SQLite::Statement isAllowedToDelete(db, "select publisher_id from posts where id=?");
+                    isAllowedToDelete.bind(1, postID);
+                    bool canDelete = false;
+                    while (isAllowedToDelete.executeStep()) {
+                        tmp = isAllowedToDelete.getColumn("publisher_id");
+                        if (tmp == UID) {
+                            canDelete = true;
+                        }
+                    }
+                    if (canDelete) {
+                        SQLite::Statement deleteStmt(db, "delete from posts where id=?");
+                        deleteStmt.bind(1, postID);
+                        deleteStmt.exec();
+                    } else {
+                        resp->SetStatus(Unauthorized);
+                        responseBody["error"] = "You are not allowed to do this";
+                    }
+                } else {
+                    resp->SetStatus(Not_Found);
+                    responseBody["error"] = "No post with ID" + std::to_string(postID) + " exist";
+                }
+            } else {
+                resp->SetStatus(Unauthorized);
+                responseBody["error"] = "You are not allowed to do this";
+            }
+        } catch (...) {
+            responseBody["error"] = "Something went wrong!";
+            resp->SetStatus(Internal_Server_Error);
+        }
+    } else {
+        resp->SetStatus(Unauthorized);
+        responseBody["error"] = "You are not allowed to do this";
+    }
+    resp->Header("Content-Type", "application/json");
+    resp->Raw(responseBody.dump());
+}
 
 int main(int argc, char **argv) {
     int port = 8081;
@@ -181,5 +306,7 @@ int main(int argc, char **argv) {
     server.Get("/api/v1/users/me", usersMe, {});
     server.Get("/api/v1/logout", Logout, {});
     server.Get("/api/v1/posts", AllPosts, {});
+    server.Get("/api/v1/:UID/posts", GetPostsByUID, {});
+    server.Get("/api/v1/delete/:postID", DeletePostByID, {});
     server.Run(port);
 }
